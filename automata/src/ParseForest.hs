0bb263c1385d142f832bda4b0cb567810885017e
skinge <ingo.skupin@gmail.com>
2016-12-22 01:32:37 +0100

    make parser more polymorphic

--- automata/src/ParseForest.hs
+++ automata/src/ParseForest.hs
@@ -4,6 +4,7 @@ module ParseForest where
 
 import Data.Tree
 import Text.Parsec
+import Data.Set as DS
 
 import Data.Tree.Pretty
 
@@ -14,38 +15,72 @@ simpleTree = do
     char ')'
     return $ Node () ts
 
+type Parser a = Parsec String () a
+data Translator a = Translator { node :: Parser a, startMark :: Parser a, endMark :: Parser a}
 
-parseTree :: Parsec String () (Tree String)
-parseTree = do
-    l <- letter
-    f <- parseForest
-    return $ Node [l] f
+tree :: (Eq a,Show a) => Translator a -> Parsec String () (Tree a)
+tree trans = do
+    l <- node trans
+    f <- forest trans
+    return $ Node l f
 
-parseForest :: Parsec String () (Forest String)
-parseForest = do
-    let isLabel c = c `elem` ['a'..'z'] ++ ['A'..'Z']
-    l <- letter <|> char '('
-    if isLabel l
-    then do
-        f <- parseForest
-        return $ [Node [l] f]
-    else do
-        ts <- many parseTree
-        char ')'
-        return ts
+forest :: (Eq a,Show a) => Translator a -> Parsec String () (Forest a)
+forest trans = do
+    check <- (fmap Right $ node trans) <|> (fmap Left $ startMark trans)
+    case check of
+        Right l -> do
+            f <- forest trans
+            return $ [Node l f]
+        Left _ -> do
+            ts <- many $ tree trans
+            endMark trans
+            return ts
+
+letters :: Translator Char
+letters = Translator letter (char '(') (char ')')
+
+forest' = forest letters
+tree' = tree letters
+
+data SomeLetters = A | B | C | D | E | F | G | H | I | J | K
+                 | L | M | N | O | P | Q | R | S | T | U | V
+                 | W | X | Y | Z | Start | End deriving (Eq,Show,Enum,Ord)
+someLetterChars :: Parsec String () SomeLetters
+someLetterChars = do
+    c <- choice $ fmap char (['a'..'z'])
+    let pairs = zip ['a'..'z'] [A .. Z]
+    return $ snd $ head $ Prelude.filter ((c ==) . fst) pairs
+someLetterStart = char '(' >> return Start
+someLetterEnd = char ')' >> return End
+someLetters :: Translator SomeLetters
+someLetters = Translator someLetterChars  someLetterStart someLetterEnd
+
+tree'' = tree someLetters
+forest'' = forest someLetters
 
 main :: IO ()
 main = do
-    case (parse simpleTree "" "(()())") of Right t -> putStr $ drawVerticalTree (fmap show t) 
+    case (parse simpleTree "" "(()())") of
+        Right t -> putStr $ drawVerticalTree (fmap show t) 
     putStr "-----\n"
-    case (parse simpleTree "" "((()(()))())") of Right t -> putStr $ drawVerticalTree (fmap show t) 
+    case (parse simpleTree "" "((()(()))())") of
+        Right t -> putStr $ drawVerticalTree (fmap show t) 
     putStr "Trees:\n"
-    case (parse parseTree "" "ab(d()ab()c())") of Right t -> putStr $ drawVerticalTree (fmap show t) 
+    case (parse tree' "" "ab(d()ab()c())") of
+        Right t -> putStr $ drawVerticalTree (fmap show t) 
+    case (parse tree'' "" "ab(d()ab()c())") of
+        Right t -> putStr $ drawVerticalTree (fmap show t) 
     putStr "-----\n"
-    case (parse parseTree "" "a(b()c()foobar())") of Right t -> putStr $ drawVerticalTree (fmap show t) 
-    putStr "Forest:\n"
-    case (parse parseForest "" "(a()b())") of Right ts -> mapM_ putStr $ map (\t -> drawVerticalTree (fmap show t)) ts
+    case (parse tree' "" "a(b()c()foobar())") of
+        Right t -> putStr $ drawVerticalTree (fmap show t) 
+    case (parse tree'' "" "a(b()c()foobar())") of
+        Right t -> putStr $ drawVerticalTree (fmap show t) 
+    putStr "Forests:\n"
+    case (parse forest' "" "(a()b())") of
+        Right ts -> mapM_ putStr $ fmap (\t -> drawVerticalTree (fmap show t)) ts
     putStr "-----\n"
-    case (parse parseForest "" "()") of Right ts -> mapM_ putStr $ map (\t -> drawVerticalTree (fmap show t)) ts
+    case (parse forest' "" "()") of
+        Right ts -> mapM_ putStr $ fmap (\t -> drawVerticalTree (fmap show t)) ts
     putStr "-----\n"
-    case (parse parseForest "" "(a()x()v())") of Right ts -> mapM_ putStr $ map (\t -> drawVerticalTree (fmap show t)) ts
+    case (parse forest' "" "(a()x()v())") of
+        Right ts -> mapM_ putStr $ fmap (\t -> drawVerticalTree (fmap show t)) ts
