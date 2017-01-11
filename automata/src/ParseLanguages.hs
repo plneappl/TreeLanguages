eaa55f2f164f38c47b9d05a97240faf68141aea4
Ingo Skupin <skupin@informatik.uni-tuebingen.de>
2017-01-11 15:07:01 +0100

    add DTD parser

--- automata/src/ParseLanguages.hs
+++ automata/src/ParseLanguages.hs
@@ -71,6 +71,40 @@ regExp unit singleton = do
          [r] -> return r
          _   -> return $ Concat rs
 
+parseDTD :: Eq a =>  Parser () -> Parser a -> Parser (DTD a)
+parseDTD unit singleton = do
+    let regex = regExp unit singleton
+        sep   = do  many (char ' ')
+                    choice [ char '\n'
+                            , char ','
+                            ]
+                    many (char ' ')
+        arrow = many (char ' ') >> string "->" >> many (char ' ')
+    r <- singleton
+    arrow
+    rreg <- regex
+    let dtd = addToDTD (DTD r (const Empty)) r rreg
+    rules <- many $ try $ do
+                many1 sep
+                s <- singleton 
+                arrow
+                rreg <- regex
+                return (s, rreg)
+    many sep >> eof
+    return $ foldr (\(s, reg) d -> addToDTD d s reg) dtd rules
+              
+
+addToDTD :: (Eq a) => DTD a -> a -> RegExp a -> DTD a
+addToDTD dtd s regex = 
+    dtd { rules = newRules }
+    where
+        newRules st =
+            if s == st
+            then case rules dtd st of
+                    Empty -> regex
+                    Union rs -> Union (rs ++ [regex])
+                    oldRegex -> Union [oldRegex, regex]
+            else rules dtd st
 
 data Chars = A | B | C | D | E | F | G | H | I | J | K | L | M
            | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
