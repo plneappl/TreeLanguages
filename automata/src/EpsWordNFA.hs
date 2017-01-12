2b182da9c324cf8056fa47dd832de4b59d23fe7b
Ingo Skupin <skupin@informatik.uni-tuebingen.de>
2017-01-12 17:30:22 +0100

    finish RegEx -> NFA conversion

--- automata/src/EpsWordNFA.hs
+++ automata/src/EpsWordNFA.hs
@@ -26,10 +26,10 @@ instance (States s, Ord s) => WordAutomaton (EpsWordNFA s) where
 
 --  runEpsWordNFA :: (Ord s, States s, HasEmptyState s) => EpsWordNFA s a -> Word a -> DS.Set s
 runEpsWordNFA :: (Ord s, States s, Alphabet a) => EpsWordNFA s a -> Word a -> DS.Set s
-runEpsWordNFA na = foldl applyTransition (start na)
+runEpsWordNFA na = foldl applyTransition (doEpsTrans $ start na)
     where
         applyTransition states letter =
-            foldMap (delta na letter) (doEpsTrans states)
+            foldMap (doEpsTrans . delta na letter) states
         doEpsTrans states = let reachable = foldMap (epsDelta na) states
                     in if DS.null (reachable DS.\\ states)
                         then states
@@ -63,11 +63,11 @@ data CountableState = CState Int deriving (Eq,Show,Ord)
 instance States CountableState where
     allStates = DS.fromList $ fmap CState [1..]
 
-fromRegExp :: (Alphabet a) => RegExp a -> EpsWordNFA CountableState a
+fromRegExp :: (Eq a, Alphabet a) => RegExp a -> EpsWordNFA CountableState a
 fromRegExp Empty = EpsWNFA (const $ const DS.empty) (const DS.empty) DS.empty DS.empty
 fromRegExp regex = fst $ fromRegExp' 0 regex
 
-fromRegExp' :: (Alphabet a) => Int -> RegExp a -> (EpsWordNFA CountableState a, Int)
+fromRegExp' :: (Eq a, Alphabet a) => Int -> RegExp a -> (EpsWordNFA CountableState a, Int)
 fromRegExp' n Empty = undefined
  -- λ : o---ε--->o
 fromRegExp' n Unit  = (EpsWNFA delta epsDelta (DS.singleton $ CState n) (DS.singleton $ CState $ n+1), totNum)
@@ -79,7 +79,7 @@ fromRegExp' n Unit  = (EpsWNFA delta epsDelta (DS.singleton $ CState n) (DS.sing
 fromRegExp' n (Singleton a) = (EpsWNFA delta epsDelta (DS.singleton $ CState n) (DS.singleton $ CState $ n+1), totNum)
     where
         totNum             = n+1
-        delta a (CState s) = if s == n then DS.singleton (CState $ s+1) else DS.empty
+        delta b (CState s) = if s == n && b == a then DS.singleton (CState $ s+1) else DS.empty
         epsDelta           = const DS.empty
  --      _____________ε_____________.
  --      |                          v
@@ -90,41 +90,76 @@ fromRegExp' n (Star regex) = (EpsWNFA delta' epsDelta' startState endState, totN
         endState           = DS.singleton $ CState $ n+1
         startState         = DS.singleton $ CState n
         (starAut, totNum)  = fromRegExp' (n+2) regex
-        delta' a (CState s)
-            | s > n+1              = delta starAut a (CState s)
+        delta' a cs@(CState s)
+            | s < n                = DS.empty
+            | s > n+1              = delta starAut a cs
             | n <= s && s <= (n+1) = DS.empty
-        epsDelta' (CState s)
-            | s == n     = start starAut `DS.union` (DS.singleton $ CState (s+1))
+        epsDelta' cs@(CState s)
+            | s < n      = DS.empty
+            | s == n     = start starAut `DS.union` DS.singleton (CState (s+1))
             | s == (n+1) = DS.empty
-            | s > (n+1)  = if (CState s) `DS.member` (acc starAut)
-                           then (epsDelta starAut (CState s)) `DS.union` (start starAut) `DS.union` endState
-                           else epsDelta starAut (CState s)
+            | s > (n+1)  = if cs `DS.member` acc starAut
+                           then epsDelta starAut cs `DS.union` start starAut `DS.union` endState
+                           else epsDelta starAut cs
  -- s + t : o--ε--N(s)--ε->o
  --         |__ε__N(t)__ε__î
 fromRegExp' n (Union rs) = (EpsWNFA delta' epsDelta' startState endState, totNum)
     where
         startState = DS.singleton $ CState n
         endState   = DS.singleton $ CState $ n+1
-        auts       = tail $ foldl (\ps r -> case ps of
-                                    [] -> [fromRegExp' (n+2) r]
-                                    xs@((a,m):as) -> (fromRegExp' (m+1) r) : xs)
+        auts       = foldl (\ps r -> case ps of
+                                [] -> [fromRegExp' (n+2) r]
+                                xs@((a,m):as) -> fromRegExp' (m+1) r : xs)
                             [] rs
+        onlyAuts   = map fst auts
         totNum     = case auts of
                         [] -> error "empty union makes no sense"
                         _  -> snd $ head auts
-        delta'     = undefined
-        epsDelta'  = undefined
+        delta' a cs@(CState s)
+            | s < n    = DS.empty
+            | s == n   = DS.empty
+            | s == n+1 = DS.empty
+            | s > n+1  = foldMap (\aut -> delta aut a cs) onlyAuts
+        epsDelta' cs@(CState s)
+            | s < n    = DS.empty
+            | s == n   = foldMap start onlyAuts
+            | s == n+1 = DS.empty
+            | s > n+1  = foldMap (\aut -> if cs `DS.member` acc aut
+                                          then epsDelta aut cs `DS.union` endState
+                                          else epsDelta aut cs)
+                            onlyAuts
  -- st : o---N(s)--N(t)-->o
 fromRegExp' n (Concat rs) = (EpsWNFA delta' epsDelta' startState endState, totNum)
     where
-        auts       = tail $ foldr (\r ps -> case ps of
-                                    [] -> [fromRegExp' (n+2) r]
-                                    xs@((a,m):as) -> (fromRegExp' (m+1) r) : xs)
-                            [] $ rs
+        lastAut    = fromRegExp' (n+2) $ last rs -- 
+        auts       = foldr (\r ps -> case ps of
+                                [] -> [lastAut]
+                                xs@((a,m):as) -> fromRegExp' (m+1) r : xs)
+                            [] rs
+        onlyAuts   = map fst auts
         totNum     = case auts of
                         [] -> error "empty concat makes no sense"
                         _  -> snd $ head auts
-        startState = undefined
-        endState   = undefined
-        delta'     = undefined
-        epsDelta'  = undefined
+        startState = DS.singleton $ CState n
+        endState   = DS.singleton $ CState $ n+1
+        delta' a cs@(CState s)
+            | s < n    = DS.empty
+            | s == n   = DS.empty
+            | s == n+1 = DS.empty
+            | s > n+1  = foldMap (\aut -> delta aut a cs) onlyAuts
+        epsDeltaAuts cs = foldMap (`epsDelta` cs) onlyAuts 
+        epsDelta' cs@(CState s) 
+            | s < n    = DS.empty
+            | s == n   = start $ head onlyAuts
+            | s == n+1 = DS.empty
+            | s > n+1  = if s <= snd lastAut
+                         then epsDeltaAuts cs `DS.union`
+                            (if cs `DS.member` acc (fst lastAut) then endState else DS.empty)
+                         else epsDeltaAuts cs `DS.union`
+                            snd (foldl (\(last, ss) (aut, m) ->
+                                        let last' = cs `DS.member` acc aut
+                                            ss'   = if last
+                                                    then ss `DS.union` start aut
+                                                    else ss
+                                        in (last', ss'))
+                                (False, DS.empty) auts)
